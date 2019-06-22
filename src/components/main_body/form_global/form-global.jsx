1476f81fec17b6e05e9f3824d53ae8d9fe6a8ca3
Pierre Gendre <pierre.gendre@wifirst.net>
2019-06-22 10:55:19 +0200

    form description

--- src/components/main_body/form_global/form-global.jsx
+++ src/components/main_body/form_global/form-global.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import countries from '../../../data/countries'
 import FormInput from '../../common/form-input'
+import formDescription from './form-description'
 
 class FormGlobal extends React.Component {
   static propTypes = {}
@@ -36,20 +37,23 @@ class FormGlobal extends React.Component {
       error: null
     }
   }
-  namesRegex = "^([a-zA-Z]+[ |'|-|-]?)+$"
-  passportNumberRegex = '^[a-zA-Z0-9]{9}$'
 
   render() {
     return (
       <div>
         <FormInput
-          placeholder="Surname"
-          pattern={this.namesRegex}
+          placeholder={formDescription.surname.placeholder}
+          pattern={formDescription.surname.regex}
           type="text"
         />
         <FormInput
-          placeholder="Passport Number ex: AV4562B56"
-          pattern={this.passportNumberRegex}
+          placeholder={formDescription.givenNames.placeholder}
+          pattern={formDescription.givenNames.regex}
+          type="text"
+        />
+        <FormInput
+          placeholder={formDescription.passportNumber.placeholder}
+          pattern={formDescription.passportNumber.regex}
           type="text"
         />
         <FormInput type="date-picker" />
