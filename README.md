787caa6ecb986ec19e9d0f34c3dfc92c027ee2a6
Pierre Gendre <pierre.gendre@wifirst.net>
2019-06-23 23:52:37 +0200

    snapshots

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ The project allows you to generate a MRZ of type TD3 threw a form completion.</b
 Data collected are sent to a frontend service that simulates a call API.</br>
 The service call the _mrz-generator_ npm module to process the computing.
 
+NB : I wanted to test the components using enzyme snapshots (especially _FormInput_).
+Nevertheless, I faced some issues: Snapshots are empty... It might from jest version (that I can't change because I haven't ejected the app)
+
 ### Installation
 
 ```
--- src/__tests__/components/common/form-input.test.js
+++ src/__tests__/components/common/form-input.test.js
@@ -1,22 +1,21 @@
-import { shallow } from 'enzyme'
-
 import React from 'react'
+
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
 
 import FormInput from '../../../components/common/form-input'
 
-console.log('FORM INPUT ===========', FormInput)
+configure({ adapter: new Adapter() })
+
 test('FORM INPUT 01 : Should render component', () => {
   let props = {
-    type: 'text',
+    type: 'select',
+    options: [
+      { label: 'label A', value: 'value A' },
+      { label: 'label B', value: 'value B' }
+    ],
     onChange: () => null
   }
-  // type="select"
-  // placeholder={formDescription.nationality.placeholder}
-  // options={countries}
-  // onChange={evt => this.handleChange(evt, 'nationality')}
-  // value={this.state.nationality.value}
-  // error={this.state.nationality.error}
-
   const wrapper = shallow(<FormInput {...props} />)
   expect(wrapper).toMatchSnapshot()
 })
